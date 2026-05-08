4881ea8fa1725932ddf93b09d43628c4d326b106
softwaredevemmanuel <48366297+softwaredevemmanuel@users.noreply.github.com>
2026-05-08 23:24:26 +0100

    user table

---
+++ src/main/java/com/bank/bankingapp/entity/User.java
@@ -0,0 +1,27 @@
+package com.bank.bankingapp.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private String role; // USER or ADMIN
+}
---
+++ src/main/java/com/bank/bankingapp/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.bank.bankingapp.repository;
+
+import com.bank.bankingapp.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
